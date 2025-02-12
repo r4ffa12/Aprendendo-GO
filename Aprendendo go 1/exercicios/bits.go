a8a76af6ab69cde3a279a9cdc28f55d4b1de8080
Rafael <rafael12hqss@gmail.com>
2025-02-12 06:49:23 -0300

    mudança de titulos ex2

--- Aprendendo go 1/exercicios/bits.go
+++ Aprendendo go 1/exercicios/bits.go
@@ -1,3 +1,9 @@
+//Crie um programa que:
+// - Atribua um valor int a uma variável
+//  - Demonstre este valor em decimal, binário e hexadecimal
+// - Desloque os bits dessa variável 1 para a esquerda, e atribua o resultado a outra variável
+// - Demonstre esta outra variável em decimal, binário e hexadecimal
+
 package main
 
 import (
